d2710f270398fba997d6b290158fa2c98726c849
MewDefi <mewdefi-github-ggRjBKJzR9JmGJwnzVLh@schildmann.net>
2021-03-03 01:21:01 +0100

    Wed Mar  3 01:21:01 AM CET 2021

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2742,7 +2742,7 @@ var baseColors = {
     primary: "#f42d7c",
     primaryBright: "#f42d7c",
     primaryDark: "#9c1c4f",
-    secondary: "#a56227",
+    secondary: "#f42d7c",
     success: "#31D0AA",
     warning: "#FFB237",
 };
@@ -2752,7 +2752,7 @@ var brandColors = {
 var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#FAF9FA", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#eeeaf4", tertiary: "#EFF4F5", text: '#9c1c4f', textDisabled: '#998c83', textSubtle: '#94073f', borderColor: "#E9EAEB", card: "#FFFFFF", gradients: {
         bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
     } });
-var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#be7434", background: "#343135", backgroundDisabled: "#3c3742", contrast: "#FFFFFF", invertedContrast: "#191326", input: "#483f5a", primaryDark: "#0098A1", tertiary: "#353547", text: "#EAE2FC", textDisabled: "#666171", textSubtle: "#c9c4d4", borderColor: "#524B63", card: "#27262c", gradients: {
+var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#f42d7c", background: "#343135", backgroundDisabled: "#3c3742", contrast: "#FFFFFF", invertedContrast: "#191326", input: "#483f5a", primaryDark: "#0098A1", tertiary: "#353547", text: "#EAE2FC", textDisabled: "#666171", textSubtle: "#c9c4d4", borderColor: "#524B63", card: "#27262c", gradients: {
         bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
     } });
 
